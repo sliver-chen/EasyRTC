aba29c06decc34ebc596a48053d621ddb7dfda3f
sliver <15505920258@163.com>
2018-12-10 13:33:57 +0800

    [iOS-feature]:create ios project and intergrated libjnigle by pod
    libjingle is a oc wraper library of webrtc.

---
+++ iOS/EasyRTC/EasyRTC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyRTC
+//
+//  Created by cx on 2018/12/10.
+//  Copyright © 2018 cx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/EasyRTC/EasyRTC/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  EasyRTC
+//
+//  Created by cx on 2018/12/10.
+//  Copyright © 2018 cx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
